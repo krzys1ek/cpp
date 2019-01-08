c2a0fe3aa6378316c46c06b38ee0f659c178be65
krzys1ek <combat100@gmail.com>
2019-01-08 21:29:05 +0000

    price_calculator - project

---
+++ price_calculator/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double num_netto(double brutto, double vat);
+
+int main()
+{
+    double netto, brutto, margin, vat;
+    
+    cout << "Amount brutto: ";
+    cin >> brutto;
+    
+    cout << "vat number: ";
+    cin >> vat;
+    
+    netto = num_netto(brutto, vat);
+    margin = netto * 1.20;
+    
+    cout << "Price netto: " << netto << endl;
+    cout << "Price after margin: " << margin << endl;
+    
+    return 0;
+}
+
+double num_netto(double brutto, double vat)
+{
+    double netto = brutto / (1+(vat/100));  // pattern for calculate netto price form brutto
+    
+    return netto;
+}
+
