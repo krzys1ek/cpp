3a8752cf797db171c387b8258d46b8450fcc5e59
krzys1ek <combat100@gmail.com>
2019-01-10 18:10:55 +0000

    deleta int j

--- christmas_tree/main.cpp
+++ christmas_tree/main.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main()
 {
-    int i, j, k;
+    int i, j;
     
     int x = 5;
     int y = 1;
     
-    for( i = 1; i <= 5; i++)
+    for( j = 1; j <= 5; j++)
     {
-        cout << "petla" << i;
-        for( j = 1; j <= x; j++)
+        cout << " ";
+        for( i = 1; i <= x; i++)
         {
             cout << " ";
         }
         x--;
         
-        for( k = 1; k <= y; k++)
+        for( i = 1; i <= y; i++)
         {
             cout << "*";
         }
